bb1b8d5732b1d289272bd18674a0a6f2eb93b01b
Bebeto03 <rnxroberto@gmail.com>
2024-01-31 16:55:55 -0300

    pagamento

---
+++ C/Lista_01(Estrutura_Sequencial)/Pagamento.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void ler_texto(char *buffer, int length) { 
+    
+    fgets(buffer, length, stdin); 
+    strtok(buffer, "\n"); 
+
+} 
+
+int main () {
+
+    char nome[50];
+
+    printf("Nome: ");
+    ler_texto(nome, 50);
+
+    double valorPorHora;
+
+    printf("Valor por hora: ");
+    scanf("%lf", &valorPorHora);
+
+    int horasTrabalhadas;
+
+    printf("Horas trabalhadas: ");
+    scanf("%d", &horasTrabalhadas);
+
+    double pagamento = valorPorHora * horasTrabalhadas;
+
+    printf("O pagamento para %s deve ser de %.2lf", nome, pagamento);
+
+}
