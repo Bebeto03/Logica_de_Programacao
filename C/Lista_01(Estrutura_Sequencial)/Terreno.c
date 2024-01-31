c36918947ef99b03c97e3a82572b8b7277184b54
Bebeto03 <rnxroberto@gmail.com>
2024-01-31 10:45:07 -0300

    terreno

---
+++ C/Lista_01(Estrutura_Sequencial)/Terreno.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+
+    double larguraTerreno, comprimentoTerreno, valorMetroQuadrado;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &larguraTerreno);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimentoTerreno);
+
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valorMetroQuadrado);
+
+    double areaTerreno = larguraTerreno * comprimentoTerreno;
+
+    double precoTerreno = areaTerreno * valorMetroQuadrado;
+
+    printf("Area do terreno = %.2lf\n", areaTerreno);
+    printf("Preco do terreno = %.2lf", precoTerreno);
+
+}
