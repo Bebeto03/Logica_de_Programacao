b444a8abc6f9a0af431f6a0e88c8ff9b523946c1
Bebeto03 <rnxroberto@gmail.com>
2024-01-31 16:30:31 -0300

    soma

---
+++ C/Lista_01(Estrutura_Sequencial)/Soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+    int x, y;
+
+    printf("Digite o valor de X: ");
+    scanf("%d", &x);
+
+    printf("Digite o valor de Y: ");
+    scanf("%d", &y);
+
+    int soma = x + y;
+
+    printf("SOMA = %d", soma);
+
+}
