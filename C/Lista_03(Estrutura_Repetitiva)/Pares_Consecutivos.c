1b2119019248f8098a2cd6961df49c063e403da0
Bebeto03 <rnxroberto@gmail.com>
2024-02-02 12:08:49 -0300

    pares consecutivos

---
+++ C/Lista_03(Estrutura_Repetitiva)/Pares_Consecutivos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main () {
+
+    int x;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &x);
+
+    int soma = 0;
+
+    while (x!=0) {
+
+        if(x%2==0){
+            soma = 5*x + 20;
+        }
+        else{
+            soma = 5*x + 25;
+        }
+
+        printf("SOMA = %d\n", soma);
+
+        printf("Digite um numero inteiro: ");
+        scanf("%d", &x);
+
+    }
+
+    return 0;
+
+}
