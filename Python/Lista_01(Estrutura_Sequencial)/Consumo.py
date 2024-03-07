89564150f6f620e47793dedd3e9af6f9bdcaebdb
Bebeto03 <rnxroberto@gmail.com>
2024-03-07 10:39:16 -0300

    consumo

---
+++ Python/Lista_01(Estrutura_Sequencial)/Consumo.py
@@ -0,0 +1,7 @@
+distanciaPercorrida = int(input("Distancia percorrida: "))
+
+combustivelGasto = float(input("Combustivel gasto: "))
+
+consumoMedio = distanciaPercorrida/combustivelGasto
+
+print(f"Consumo medio = {consumoMedio:.3f}")
