7209893681b16f5cfbc6d4495822f21f4f9331e6
Bebeto03 <rnxroberto@gmail.com>
2024-03-14 15:44:44 -0300

    negativos

---
+++ Python/Lista_04(Vetores)/Negativos.py
@@ -0,0 +1,12 @@
+n = int(input("Quantos numeros voce vai digitar? "))
+
+vet = [0 for x in range(0,n)]
+
+for i in range(0,n):
+    vet[i] = int(input("Digite um numero: "))
+
+print("NUMEROS NEGATIVOS:")
+
+for i in range(0,n):
+    if vet[i]<0:
+        print(vet[i])
