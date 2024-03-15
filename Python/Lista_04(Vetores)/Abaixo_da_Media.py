6a7608e9136fe75a7d13ffdd4df91c47bca04a6b
Bebeto03 <rnxroberto@gmail.com>
2024-03-15 20:08:14 -0300

    abaixo da media

---
+++ Python/Lista_04(Vetores)/Abaixo_da_Media.py
@@ -0,0 +1,20 @@
+n = int(input("Quantos elementos vai ter o vetor? "))
+
+vet = [0 for x in range(n)]
+
+for i in range(0,n):
+    vet[i] = float(input("Digite um numero: "))
+
+soma = 0.0
+
+for i in range(0,n):
+    soma+=vet[i]
+
+media = soma/n
+
+print(f"\nMEDIA DO VETOR = {media:.3f}")
+
+print("ELEMENTOS ABAIXO DA MEDIA:")
+for i in range(0,n):
+    if vet[i]<media:
+        print(f"{vet[i]:.1f}")
