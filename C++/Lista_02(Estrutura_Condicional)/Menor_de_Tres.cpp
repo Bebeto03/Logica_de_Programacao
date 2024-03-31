69a49306e7ca4d35a4dbbcd5b519785ac2b516be
Bebeto03 <rnxroberto@gmail.com>
2024-03-31 15:09:05 -0300

    menor de tres

---
+++ C++/Lista_02(Estrutura_Condicional)/Menor_de_Tres.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    int valor1, valor2, valor3, menor;
+
+    cout << "Primeiro valor: ";
+    cin >> valor1;
+
+    cout << "Segundo valor: ";
+    cin >> valor2;
+
+    cout << "Terceiro valor: ";
+    cin >> valor3;
+
+    if (valor1<=valor2 && valor1<=valor3){
+        menor = valor1;
+    }
+    else if (valor2<=valor1 && valor2<= valor3){
+        menor = valor2;
+    }
+    else{
+        menor = valor3;
+    }
+
+    cout << "MENOR = " << menor;
+
+    return 0;
+
+}
