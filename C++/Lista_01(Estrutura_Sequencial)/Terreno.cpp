990b390ec213c7af56e4cb46e6396a7a65a7a8e0
Bebeto03 <rnxroberto@gmail.com>
2024-03-30 23:10:30 -0300

    terreno

---
+++ C++/Lista_01(Estrutura_Sequencial)/Terreno.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    double largura, comprimento, area;
+    double valorMetroQuadrado, preco;
+
+    cout << "Digite a largura do terreno: ";
+    cin >> largura;
+
+    cout << "Digite o comprimento do terreno: ";
+    cin >> comprimento;
+
+    cout << "Digite o valor do metro quadrado: ";
+    cin >> valorMetroQuadrado;
+
+    area = largura*comprimento;
+
+    preco = area * valorMetroQuadrado;
+
+    cout << fixed << setprecision(2);
+    cout << "Area do terreno = " << area << endl;
+    cout << "Preco do terreno = " << preco;
+
+    return 0;
+
+}
