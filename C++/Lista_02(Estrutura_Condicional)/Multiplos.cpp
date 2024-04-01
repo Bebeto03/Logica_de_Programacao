ccc3caca5e1c8780973048902e33465369e79074
Bebeto03 <rnxroberto@gmail.com>
2024-03-31 22:57:08 -0300

    multiplos

---
+++ C++/Lista_02(Estrutura_Condicional)/Multiplos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n1, n2;
+
+    cout << "Digite dois numeros inteiros:" << endl;
+    cin >> n1;
+    cin >> n2;
+
+    if (n1%n2==0 || n2%n1==0){
+        cout << "Sao multiplos";
+    }
+    else{
+        cout << "Nao sao multiplos";
+    }
+
+    return 0;
+
+}
