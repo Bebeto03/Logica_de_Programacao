74e9157195c829887366e3edf82e1c247914404c
Bebeto03 <rnxroberto@gmail.com>
2024-04-04 11:07:56 -0300

    dados pessoas

--- C++/Lista_04(Vetores)/Dados_Pessoas.cpp
+++ C++/Lista_04(Vetores)/Dados_Pessoas.cpp
@@ -13,6 +13,47 @@ int main(){
     double altura[n];
     char genero[n];
 
-    
+    for(int i=0;i<n;i++){
+        cout << "Altura da " << (i+1) << "a pessoa: ";
+        cin >> altura[i];
+        cout << "Genero da " << (i+1) << "a pessoa: ";
+        cin >> genero[i];
+    }
 
+    double menor=altura[0], maior=altura[0];
+
+    for(int i=0;i<n;i++){
+        if(altura[i]<menor){
+            menor = altura[i];
+        }
+        if(altura[i]>maior){
+            maior = altura[i];
+        }
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "Menor altura = " << menor << endl;
+    cout << "Maior altura = " << maior << endl;
+
+    double somaF=0.0;
+    int contF=0, contM=0;
+
+    for(int i=0;i<n;i++){
+        if(genero[i]=='F'){
+            contF++;
+            somaF+=altura[i];
+        }
+        if(genero[i]=='M'){
+            contM++;
+        }
+    }
+
+    double mediaF = somaF/contF;
+
+    cout << "Media das alturas das mulheres = " << fixed << setprecision(2) << mediaF << endl;
+
+    cout << "Numero de homens = " << contM;
+
+    return 0;
+    
 }
