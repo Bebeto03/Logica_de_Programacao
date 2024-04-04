9bc7e2c95f7520b41b1c81bcd8cbbdf83e5e5912
Bebeto03 <rnxroberto@gmail.com>
2024-04-04 09:39:00 -0300

    mais velho

---
+++ C++/Lista_04(Vetores)/Mais_Velho.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Quantas pessoas voce vai digitar? ";
+    cin >> n;
+
+    string nome[n];
+    int idade[n];
+
+    for(int i=0;i<n;i++){
+        cout << "Dados da " << (i+1) << "a pessoa:" << endl;
+        cout << "Nome: ";
+        cin >> nome[i];
+        cout << "Idade: ";
+        cin >> idade[i];
+    }
+
+    int maisVelho = idade[0];
+    string nomeDoMaisVelho = nome[0];
+
+    for(int i=0;i<n;i++){
+        if(idade[i]>maisVelho){
+            maisVelho = idade[i];
+            nomeDoMaisVelho = nome[i];
+        }
+    }
+
+    cout << "PESSOA MAIS VELHA: " << nomeDoMaisVelho;
+
+    return 0;
+
+}
