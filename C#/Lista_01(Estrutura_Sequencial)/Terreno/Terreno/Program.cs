6d670e6795d0dfa6e8c6de8e217fc844eea3e3a6
Bebeto03 <rnxroberto@gmail.com>
2024-05-02 14:03:42 -0300

    terreno

---
+++ C#/Lista_01(Estrutura_Sequencial)/Terreno/Terreno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Terreno{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a largura do terreno: ");
+            double largura = double.Parse(Console.ReadLine(), ci);
+
+            Console.Write("Digite o comprimento do terreno: ");
+            double comprimento = double.Parse(Console.ReadLine(), ci);
+
+            Console.Write("Digite o valor do metro quadrado: ");
+            double valorMetroQuadrado = double.Parse(Console.ReadLine(), ci);
+
+            double area = largura * comprimento;
+            double precoTerreno = area * valorMetroQuadrado;
+
+            Console.WriteLine("Area do terreno = " + area.ToString("F2", ci));
+            Console.WriteLine("Preco do terreno = " + precoTerreno.ToString("F2", ci));
+
+        }
+
+    }
+
+}
