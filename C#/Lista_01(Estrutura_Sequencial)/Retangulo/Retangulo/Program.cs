a64a3099b71cce35a305ff5785e349693c23ff99
Bebeto03 <rnxroberto@gmail.com>
2024-05-06 14:44:26 -0300

    aaaaa

---
+++ C#/Lista_01(Estrutura_Sequencial)/Retangulo/Retangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Base do retangulo: ");
+            double baseRetangulo = double.Parse(Console.ReadLine(), ci);
+
+            Console.Write("Altura do retangulo: ");
+            double alturaRetangulo = double.Parse(Console.ReadLine(), ci);
+
+            double area = baseRetangulo * alturaRetangulo;
+
+            double perimetro = 2*baseRetangulo+2*alturaRetangulo;
+
+            double diagonal = Math.Sqrt(Math.Pow(baseRetangulo,2)+Math.Pow(alturaRetangulo,2));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", ci));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", ci));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", ci));
+
+        }
+
+    }
+
+}
---
+++ olaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace olaMundo{
+
+    class Program{
+        static void Main(string[] args){
+
+            Console.WriteLine("Ola, mundo!");
+            
+        }
+
+    }
+
+}
