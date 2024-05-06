0a70badf868628b7602636d31b8991c7c5250604
Bebeto03 <rnxroberto@gmail.com>
2024-05-06 15:13:17 -0300

    soma

---
+++ C#/Lista_01(Estrutura_Sequencial)/Soma/Soma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Soma{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Digite o valor de X: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int soma = x+y;
+
+            Console.Write("SOMA = " + soma);
+
+        }
+
+    }
+    
+}
