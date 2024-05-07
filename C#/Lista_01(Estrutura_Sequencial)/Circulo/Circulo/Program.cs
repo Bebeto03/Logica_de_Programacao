9022a634637bfa2c8784e39bdd8555c44f1ee0cf
Bebeto03 <rnxroberto@gmail.com>
2024-05-07 14:14:14 -0300

    circulo

---
+++ C#/Lista_01(Estrutura_Sequencial)/Circulo/Circulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Circulo{
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite o valor do raio do circulo: ");
+            double raio = double.Parse(Console.ReadLine(),ci);
+
+            double area = Math.PI * Math.Pow(raio,2);
+
+            Console.Write("AREA = " + area.ToString("F3",ci));
+
+        }
+
+    }
+    
+}
