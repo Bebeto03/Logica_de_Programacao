6454040d04661a64a9f6f10c729e2fd895ee7288
Bebeto03 <rnxroberto@gmail.com>
2024-05-07 13:51:36 -0300

    troco

---
+++ C#/Lista_01(Estrutura_Sequencial)/Troco/Troco/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Troco{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Preco unitario do produto: ");
+            double precoUnitario = double.Parse(Console.ReadLine(),ci);
+
+            Console.Write("Quantidade comprada: ");
+            int quantidade = int.Parse(Console.ReadLine(),ci);
+
+            Console.Write("Dinheiro recebido: ");
+            double valorRecebido = double.Parse(Console.ReadLine(),ci);
+
+            double troco = valorRecebido-precoUnitario*quantidade;
+
+            Console.Write("TROCO = " + troco.ToString("F2",ci));
+
+        }
+    }
+}
