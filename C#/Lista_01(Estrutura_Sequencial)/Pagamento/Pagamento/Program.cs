6ab6d23d284b585f57bcef13c0e01db94c226e9d
Bebeto03 <rnxroberto@gmail.com>
2024-05-08 14:05:10 -0300

    pagamento

---
+++ C#/Lista_01(Estrutura_Sequencial)/Pagamento/Pagamento/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pagamento{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Valor por hora: ");
+            double valorPorHora = double.Parse(Console.ReadLine(), ci);
+
+            Console.Write("Horas trabalhadas: ");
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+
+            double pagamento = valorPorHora * horasTrabalhadas;
+
+            Console.Write("O pagamento para " + nome + " deve ser " + pagamento.ToString("F2",ci));
+
+        }
+
+    }
+
+}
