2d5c14e93cb87f2e7631fad0cdcdcc55c812b5ea
Bebeto03 <rnxroberto@gmail.com>
2024-05-08 14:26:12 -0300

    consumo

---
+++ C#/Lista_01(Estrutura_Sequencial)/Consumo/Consumo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Distancia percorrida: ");
+            int distancia = int.Parse(Console.ReadLine());
+
+            Console.Write("Combustível gasto: ");
+            double combustivelGasto = double.Parse(Console.ReadLine(),ci);
+
+            double consumoMedio = distancia/combustivelGasto;
+
+            Console.Write("Consumo medio = " + consumoMedio.ToString("F3",ci));
+
+        }
+
+    }
+    
+}
