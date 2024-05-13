2d2d157ab2a3785307a1a4164c7a07ccc5b415a9
Bebeto03 <rnxroberto@gmail.com>
2024-05-13 12:22:34 -0300

    medidas

---
+++ C#/Lista_01(Estrutura_Sequencial)/Medidas/Medidas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Medidas{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a medida A: ");
+            double a  = double.Parse(Console.ReadLine(),ci);
+
+            Console.Write("Digite a medida B: ");
+            double b = double.Parse(Console.ReadLine(),ci);
+
+            Console.Write("Digite a medida C: ");
+            double c = double.Parse(Console.ReadLine(),ci);
+
+            double quadrado = Math.Pow(a,2);
+
+            double triangulo = a*b/2;
+
+            double trapezio = (a+b)*c/2;
+
+            Console.WriteLine("AREA DO QUADRADO = " + quadrado.ToString("F4",ci));
+            Console.WriteLine("AREA DO TRAINGULO = " + triangulo.ToString("F4",ci));
+            Console.Write("AREA DO TRAPEZIO = " + trapezio.ToString("F4",ci));
+
+        }
+
+    }
+    
+}
