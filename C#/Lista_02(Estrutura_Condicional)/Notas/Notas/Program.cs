c5fa66a0846cc20b3450fce39fb04f2f74854606
Bebeto03 <rnxroberto@gmail.com>
2024-05-23 12:31:17 -0300

    notas

---
+++ C#/Lista_02(Estrutura_Condicional)/Notas/Notas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Notas{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a primeira nota: ");
+            float n1 = float.Parse(Console.ReadLine(),ci);
+
+            Console.Write("Digite a segunda nota: ");
+            float n2 = float.Parse(Console.ReadLine(),ci);
+
+            float nF = n1+n2;
+
+            Console.WriteLine("NOTA FINAL = " + nF.ToString("F1",ci));
+
+            if(nF<60.00){
+                Console.Write("REPROVADO");
+            }
+
+        }
+
+    }
+    
+}
