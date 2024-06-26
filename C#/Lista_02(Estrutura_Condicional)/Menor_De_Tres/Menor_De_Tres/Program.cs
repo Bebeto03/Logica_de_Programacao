894d57b295b06103ac6d1bd504089f54bd161e94
Bebeto03 <rnxroberto@gmail.com>
2024-06-26 17:57:52 -0300

    menor de tres

---
+++ C#/Lista_02(Estrutura_Condicional)/Menor_De_Tres/Menor_De_Tres/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Menor_De_Tres{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Primeiro valor: ");
+            int v1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Terceiro valor: ");
+            int v3 = int.Parse(Console.ReadLine());
+
+            int menor;
+
+            if(v1<=v2 && v1<=v3){
+                menor = v1;
+            }
+            else if(v2<=v1 && v2<=v3){
+                menor = v2;
+            }
+            else{
+                menor = v3;
+            }
+
+            Console.Write("MENOR = " + menor.ToString());
+
+        }
+
+    }
+    
+}
