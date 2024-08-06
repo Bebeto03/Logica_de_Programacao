e0e67435502cc9e0ceffa7e06ece328dd5ddcf79
Bebeto03 <rnxroberto@gmail.com>
2024-08-06 14:45:55 -0300

    glicose

---
+++ C#/Lista_02(Estrutura_Condicional)/Glicose/Glicose/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Glicose{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a medida da glicose: ");
+            double glicose = double.Parse(Console.ReadLine(),ci);
+
+            string classificacao;
+
+            if(glicose<=100.0){
+                classificacao = "normal";
+            }
+            else if(glicose<=140.0){
+                classificacao = "elevado";
+            }
+            else{
+                classificacao = "diabetes";
+            }
+
+            Console.Write("Classificacao: " + classificacao);
+
+        }
+    }
+}
