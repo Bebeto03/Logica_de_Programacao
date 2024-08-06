5f0ac71533e13b3a22057e9b694ddd42165b842e
Bebeto03 <rnxroberto@gmail.com>
2024-08-06 13:59:42 -0300

    operadora

---
+++ C#/Lista_02(Estrutura_Condicional)/Operadora/Operadora/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Operadora{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a quantidade de minutos: ");
+            int minutos = int.Parse(Console.ReadLine());
+
+            double aPagar;
+
+            if (minutos<=100){
+                aPagar = 50.00;
+            }
+            else{
+                aPagar = 50.00 + (minutos-100)*2;
+            }
+
+            Console.Write("Valor a pagar: R$ " + aPagar.ToString("F2",ci));
+
+        }
+    }
+}
