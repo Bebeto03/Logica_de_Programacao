177f82511b6b5246cd13fbfce92a6e3a40e902c5
Bebeto03 <rnxroberto@gmail.com>
2024-08-13 15:56:03 -0300

    multiplos

---
+++ C#/Lista_02(Estrutura_Condicional)/Multiplos/Multiplos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Multiplos{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Digite dois numeros inteiros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int n3;
+
+            if(n1 < n2){
+                n3 = n1;
+                n1 = n2;
+                n2 = n3;
+            }
+
+            if(n1%n2 == 0){
+                Console.WriteLine("Sao multiplos");
+            }
+            else{
+                Console.WriteLine("Nao sao multiplos");
+            }
+
+        }
+    }
+}
