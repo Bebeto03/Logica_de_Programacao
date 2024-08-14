bd1b55867a67fdd229f3ea589cf142bd107139ba
Bebeto03 <rnxroberto@gmail.com>
2024-08-14 10:34:46 -0300

    tempo de jogo

---
+++ C#/Lista_02(Estrutura_Condicional)/Tempo_De_Jogo/Tempo_De_Jogo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tempo_De_Jogo{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Hora inicial: ");
+            int horaI = int.Parse(Console.ReadLine());
+
+            Console.Write("Hora final: ");
+            int horaF = int.Parse(Console.ReadLine());
+
+            if(horaF <= horaI){
+                horaF = horaF + 24;
+            }
+
+            int duracao = horaF - horaI;
+
+            Console.Write("O JOGO DUROU " + duracao + " HORA(S)");
+
+        }
+
+    }
+    
+}
