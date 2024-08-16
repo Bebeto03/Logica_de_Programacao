b888ccee7e4f2c2c547dc00743cbf2c1c7a69581
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 19:08:54 -0300

    pares consecutivos

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Pares_Consecutivos/Pares_Consecutivos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pares_Consecutivos{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Digite um numero inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while(n!=0){
+                if(n%2==0){
+                    Console.WriteLine("SOMA = " + (5*n+20));
+                }
+                else{
+                    Console.WriteLine("SOMA = " + (5*n+25));
+                }
+                Console.Write("Digite um numero inteiro: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+        }
+        
+    }
+
+}
