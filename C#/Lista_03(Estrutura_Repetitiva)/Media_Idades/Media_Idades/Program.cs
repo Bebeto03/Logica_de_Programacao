cd62f6cc4bd11d06a7da0943214e8a19a986bcd0
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 17:22:51 -0300

    media idades

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Media_Idades/Media_Idades/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media_Idades{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Digite as idades:");
+            int idade = int.Parse(Console.ReadLine());
+
+            int soma=0, cont=0;
+
+            while(idade>=0){
+                cont++;
+                soma+=idade;
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            if(cont==0){
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }
+            else{
+                double media = (double)soma/cont;
+                Console.WriteLine("MEDIA = "+ media.ToString("F2",ci));
+            }
+
+        }
+
+    }
+    
+}
