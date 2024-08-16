1ad8359cc1f5a5d9f88cd10fa17f29595acdcd21
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 16:54:30 -0300

    crescente

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Crescente/Crescente/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Crescente{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Digite dois numeros: ");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            while(x != y){
+                if(x < y){
+                    Console.WriteLine("Crescente!");
+                }
+                else{
+                    Console.WriteLine("Decrescente!");
+                }
+                Console.WriteLine("Digite outros dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+
+        }
+
+    }
+    
+}
