2183b93ce8e97f506fa036942e3b5e6878c58d0b
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 18:31:45 -0300

    quadrante

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Quadrante/Quadrante/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Quadrante{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Digite os valores das coordenadas X e Y:");
+            double x = double.Parse(Console.ReadLine(),ci);
+            double y = double.Parse(Console.ReadLine(),ci);
+
+            while(x!=0 && y!=0){
+                if(x>0 && y>0){
+                    Console.WriteLine("Quadrante Q1");
+                }
+                else if(x<0 && y>0){
+                    Console.WriteLine("Quadrante Q2");
+                }
+                else if(x<0 && y<0){
+                    Console.WriteLine("Quadrante Q3");
+                }
+                else{
+                    Console.WriteLine("Quadrante Q4");
+                }
+                Console.WriteLine("Digite os valores das coordenadas X e Y:");
+                x = double.Parse(Console.ReadLine(),ci);
+                y = double.Parse(Console.ReadLine(),ci);
+            }
+
+        }
+
+    }
+    
+}
