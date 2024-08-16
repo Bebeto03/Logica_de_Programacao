1f2da639f7a7a5ef05bb961f1f8ce447d6a6e513
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 17:30:15 -0300

    senha fixa

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Senha_Fixa/Senha_Fixa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Senha_Fixa{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Digite a senha: ");
+            string senha = Console.ReadLine();
+
+            while(senha != "2002"){
+                Console.Write("Senha invalida! Tente novamente: ");
+                senha = Console.ReadLine();
+            }
+
+            Console.Write("Acesso permitido.");
+
+        }
+
+    }
+    
+}
