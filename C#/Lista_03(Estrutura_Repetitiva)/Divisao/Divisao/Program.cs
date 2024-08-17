32cb6c207e2add64a61ed0f614ff677ee287117f
Bebeto03 <rnxroberto@gmail.com>
2024-08-17 14:34:36 -0300

    divisao

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Divisao/Divisao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Divisao{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Quantos casos voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++){
+
+                Console.Write("Entre com o numerador: ");
+                double num = double.Parse(Console.ReadLine(),ci);
+
+                Console.Write("Entre com o denominador: ");
+                double den = double.Parse(Console.ReadLine(),ci);
+
+                if(den == 0){
+                    Console.WriteLine("DIVISAO IMPOSSIVEL");
+                }
+                else{
+                    double div = num/den;
+                    Console.WriteLine("DIVISAO = " + div.ToString("F2",ci));
+                }
+
+            }
+
+        }
+
+    }
+    
+}
