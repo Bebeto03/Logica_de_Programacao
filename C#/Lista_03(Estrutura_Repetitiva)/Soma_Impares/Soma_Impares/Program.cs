2415baf090901931cdbf5211c8952fcde399ce07
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 22:51:41 -0300

    soma impares

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Soma_Impares/Soma_Impares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Soma_Impares{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Digite dois numeros:");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x > y){
+                int z = x;
+                x = y;
+                y = z;
+            }
+
+            int soma = 0;
+
+            for(int i=x+1; i<y; i++){
+                if(i % 2 != 0){
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine("SOMA DOS IMPARES = " + soma);
+        }
+    }
+}
