fe7ea21976344c0f40cbc62d78a5a747b104da5e
Bebeto03 <rnxroberto@gmail.com>
2024-08-17 14:43:18 -0300

    fatorial

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Fatorial/Fatorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fatorial{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Digite o valor de N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+
+            if(n==0 || n==1){
+                fat = 1;
+            }
+            else{
+                for(int i=n;i>0;i--){
+                    fat *= i;
+                }
+            }
+
+            Console.Write("FATORIAL = " + fat);
+
+        }
+
+    }
+    
+}
