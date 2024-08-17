5d5988072ec60a8b786b14f690ad1af3d27d5931
Bebeto03 <rnxroberto@gmail.com>
2024-08-17 14:05:30 -0300

    par impar

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Par_Impar/Par_Impar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Par_Impar{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++){
+
+                Console.Write("Digite um numero: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if(x == 0){
+                    Console.WriteLine("NULO");
+                }
+                else if(x > 0 && x % 2 == 0){
+                    Console.WriteLine("PAR POSITIVO");
+                }
+                else if(x > 0 && x % 2 !=0){
+                    Console.WriteLine("IMPAR POSITIVO");
+                }
+                else if(x < 0 && x % 2 == 0){
+                    Console.WriteLine("PAR NEGATIVO");
+                }
+                else{
+                    Console.WriteLine("IMPAR NEGATIVO");
+                }
+
+            }
+
+        }
+
+    }
+    
+}
