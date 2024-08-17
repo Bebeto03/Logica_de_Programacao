1432840bdb1b14251aea987eae5718bb6347a4b8
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 23:17:32 -0300

    dentro fora

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Dentro_Fora/Dentro_Fora/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dentro_Fora{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int dentro=0, fora=0;
+
+            for(int i=0;i<n;i++){
+                Console.Write("Digite um numero: ");
+                int x = int.Parse(Console.ReadLine());
+                if(x>=10 && x<=20){
+                    dentro++;
+                }
+                else{
+                    fora++;
+                }
+            }
+
+            Console.WriteLine(dentro + " DENTRO");
+            Console.Write(fora + " FORA");
+
+        }
+
+    }
+
+}
