4ff1b976c6212a63f96ccb1fded39b9840f5c927
Bebeto03 <rnxroberto@gmail.com>
2024-08-16 23:02:35 -0300

    sequencia impares

---
+++ C#/Lista_03(Estrutura_Repetitiva)/Sequencia_Impares/Sequencia_Impares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sequencia_Impares{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Digite o valor de X: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<=x;i++){
+                if(i%2!=0){
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+    }
+    
+}
