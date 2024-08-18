38e62ca66949ddd0f3ed8259c8541686b678da45
Bebeto03 <rnxroberto@gmail.com>
2024-08-18 12:04:14 -0300

    acima diagonal

---
+++ C#/Lista_05(Matrizes)/Acima_Diagonal/Acima_Diagonal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Acima_Diagonal{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Qual a ordem da matriz? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+
+            for(int i=0;i<n;i++){
+
+                for(int j=0;j<n;j++){
+
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i,j] = int.Parse(Console.ReadLine());
+
+                }
+
+            }
+
+            int soma = 0;
+
+            for(int i=0;i<n;i++){
+
+                for(int j=0;j<n;j++){
+
+                    if(j>i){
+                        soma += mat[i,j];
+                    }
+
+                }
+
+            }
+
+            Console.Write("SOMA DOS ELEMENTOS ACIMA DA DIAGONAL PRINCIPAL = " + soma);
+
+        }
+
+    }
+
+}
