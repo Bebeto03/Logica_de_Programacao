b9d6b8758630efcc24ae83484b80d9ae5c765cef
Bebeto03 <rnxroberto@gmail.com>
2024-08-17 22:42:21 -0300

    media pares

---
+++ C#/Lista_04(Vetores)/Media_Pares/Media_Pares/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media_Pares{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Quantos elementos vai ter o vetor? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            for(int i=0;i<vet.Length;i++){
+
+                Console.Write("Digite um numero: ");
+                vet[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            int soma = 0, cont = 0;
+
+            for(int i=0;i<vet.Length;i++){
+
+                if(vet[i] % 2 == 0){
+                    soma += vet[i];
+                    cont++;
+                }
+
+            }
+
+            if(cont==0){
+                Console.WriteLine("NENHUM NUMERO PAR");
+            }
+            else{
+                double media = (double)soma/cont;
+                Console.WriteLine("MEDIA DOS PARES = " + media.ToString("F1",ci));
+            }
+
+        }
+    }
+}
